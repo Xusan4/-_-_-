d2ccf8bb305485d9ba86998f4ddc889654eb052d
Xusan <gazievxusan777@gmail.com>
2024-05-02 10:07:11 +0500

    finished task in lesson10.py

--- lesson10.py
+++ lesson10.py
@@ -4,4 +4,11 @@ lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
-data.head()
+# print(data.head())
+data.loc[data['whoAmI'] == 'robot', 'robot'] = '1'
+data.loc[data['whoAmI'] == 'human', 'robot'] = '0'
+
+data.loc[data['whoAmI'] == 'robot', 'human'] = '0'
+data.loc[data['whoAmI'] == 'human', 'human'] = '1'
+
+print(data.head())
